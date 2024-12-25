acac3ed3efb994dec47b7a8e272a336b51771fb0
Nour Eleslem Ganzoui <noureleslem.ganzoui@isgb.ucar.tn>
2024-12-25 18:36:54 +0100

    Add files via upload

---
+++ calculatrice2.Designer.vb
@@ -0,0 +1,258 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class calculatrice2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        bt0 = New Button()
+        bt1 = New Button()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(240, 72)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(386, 27)
+        TextBox1.TabIndex = 0
+        ' 
+        ' bt0
+        ' 
+        bt0.BackColor = Color.LightPink
+        bt0.Location = New Point(217, 137)
+        bt0.Name = "bt0"
+        bt0.Size = New Size(94, 48)
+        bt0.TabIndex = 1
+        bt0.Text = "0"
+        bt0.UseVisualStyleBackColor = False
+        ' 
+        ' bt1
+        ' 
+        bt1.BackColor = Color.LightPink
+        bt1.Location = New Point(338, 137)
+        bt1.Name = "bt1"
+        bt1.Size = New Size(94, 48)
+        bt1.TabIndex = 2
+        bt1.Text = "1"
+        bt1.UseVisualStyleBackColor = False
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.LightPink
+        Button1.Location = New Point(475, 137)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 48)
+        Button1.TabIndex = 3
+        Button1.Text = "2"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.LightPink
+        Button2.Location = New Point(338, 201)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 41)
+        Button2.TabIndex = 4
+        Button2.Text = "5"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.LightPink
+        Button3.Location = New Point(475, 191)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 51)
+        Button3.TabIndex = 5
+        Button3.Text = "6"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.LightPink
+        Button4.Location = New Point(217, 191)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(94, 47)
+        Button4.TabIndex = 6
+        Button4.Text = "4"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = Color.LightPink
+        Button5.Location = New Point(217, 267)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(94, 41)
+        Button5.TabIndex = 7
+        Button5.Text = "8"
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' Button6
+        ' 
+        Button6.BackColor = Color.LightPink
+        Button6.Location = New Point(338, 267)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(94, 41)
+        Button6.TabIndex = 8
+        Button6.Text = "9"
+        Button6.UseVisualStyleBackColor = False
+        ' 
+        ' Button7
+        ' 
+        Button7.BackColor = Color.LightPink
+        Button7.Location = New Point(475, 267)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(94, 41)
+        Button7.TabIndex = 9
+        Button7.Text = "c"
+        Button7.UseVisualStyleBackColor = False
+        ' 
+        ' Button8
+        ' 
+        Button8.BackColor = Color.LightPink
+        Button8.Location = New Point(206, 342)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(94, 29)
+        Button8.TabIndex = 10
+        Button8.Text = "+"
+        Button8.UseVisualStyleBackColor = False
+        ' 
+        ' Button9
+        ' 
+        Button9.BackColor = Color.LightPink
+        Button9.Location = New Point(598, 267)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(94, 41)
+        Button9.TabIndex = 11
+        Button9.Text = "="
+        Button9.UseVisualStyleBackColor = False
+        ' 
+        ' Button10
+        ' 
+        Button10.BackColor = Color.LightPink
+        Button10.Location = New Point(598, 191)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(94, 49)
+        Button10.TabIndex = 12
+        Button10.Text = "7"
+        Button10.UseVisualStyleBackColor = False
+        ' 
+        ' Button11
+        ' 
+        Button11.BackColor = Color.LightPink
+        Button11.Location = New Point(598, 137)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(94, 48)
+        Button11.TabIndex = 13
+        Button11.Text = "3"
+        Button11.UseVisualStyleBackColor = False
+        ' 
+        ' Button12
+        ' 
+        Button12.BackColor = Color.LightPink
+        Button12.Location = New Point(338, 336)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(94, 41)
+        Button12.TabIndex = 14
+        Button12.Text = "-"
+        Button12.UseVisualStyleBackColor = False
+        ' 
+        ' Button13
+        ' 
+        Button13.BackColor = Color.LightPink
+        Button13.Location = New Point(475, 336)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(94, 35)
+        Button13.TabIndex = 15
+        Button13.Text = "*"
+        Button13.UseVisualStyleBackColor = False
+        ' 
+        ' Button14
+        ' 
+        Button14.BackColor = Color.LightPink
+        Button14.Location = New Point(598, 331)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(94, 40)
+        Button14.TabIndex = 16
+        Button14.Text = "/"
+        Button14.UseVisualStyleBackColor = False
+        ' 
+        ' calculatrice2
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.DimGray
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Button12)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(bt1)
+        Controls.Add(bt0)
+        Controls.Add(TextBox1)
+        Name = "calculatrice2"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents bt0 As Button
+    Friend WithEvents bt1 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+
+End Class
